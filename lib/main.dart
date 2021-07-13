a0cbb82636074d69cdf3718b88ca87488a7abcbc
hamzaubit <syedjgomi@gmail.com>
2021-07-13 16:26:59 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.indigo[900],
+          title: Text("Practice App",),
+        ),
+      ),
+    );
+  }
+}
+
